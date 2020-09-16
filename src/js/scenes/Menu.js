06670d1d137d85b0927937eb0c14bc01e561d3ae
gerwant <damian.gorzelanczyk@gmail.com>
2020-09-16 23:06:07 +0200

    :lipstick: Buttons fixed

--- src/js/scenes/Menu.js
+++ src/js/scenes/Menu.js
@@ -2,56 +2,59 @@ import Phaser from 'phaser';
 import { Buttons, Label } from 'phaser3-rex-plugins/templates/ui/ui-components.js';
 import RoundRectangle from 'phaser3-rex-plugins/plugins/roundrectangle.js';
 
-const COLOR_LIGHT = 0x7b5e57;
+const COLOR_LIGHT = 0xFF00FF;
 
-let createButton = function (scene, text) {
-    return new Label(scene, {
-        width: 40,
-        height: 40,
-        background: new RoundRectangle(scene, 0, 0, 0, 0, 20, COLOR_LIGHT),
-        text: scene.add.text(0, 0, text, {
-            fontSize: 18
-        }),
-        space: {
-            left: 10,
-            right: 10,
-        },
-        align: 'center'
-    });
-}
+const createButton = function (scene, text) {
+  const rr = new RoundRectangle(scene, 0, 0, 0, 0, 20, COLOR_LIGHT);
+  return new Label(scene, {
+    width: 40,
+    height: 40,
+    background: scene.add.existing(rr),
+    text: scene.add.text(0, 0, text, {
+      fontSize: 18,
+    }),
+    space: {
+      left: 10,
+      right: 10,
+    },
+    align: 'center',
+  });
+};
 
 class Menu extends Phaser.Scene {
+  constructor() {
+    super({
+      key: 'Menu',
+    });
+  }
 
-    constructor() {
-        super({ 
-            key: 'Menu',
-        });
-    }
+  preload() {
 
-    preload() {
-        
-    }
+  }
 
-    create() {
-        let expand = true;
-        let buttons = new Buttons(this, {
-            x: 400, y: 300,
-            width: 300,
-            orientation: 'x',
+  create() {
+    const expand = true;
+    const buttons = new Buttons(this, {
+      x: 400,
+      y: 300,
+      width: 300,
+      orientation: 'x',
 
-            buttons: [
-                createButton(this, 'A'),
-                createButton(this, 'B'),
-                createButton(this, 'c'),
-            ],
+      buttons: [
+        createButton(this, 'A'),
+        createButton(this, 'B'),
+        createButton(this, 'c'),
+      ],
 
-            expand: expand
-        }).layout()
-    }
+      expand,
+    }).layout();
 
-    update() {
+    this.add.existing(buttons);
+  }
 
-    }
+  update() {
+
+  }
 }
 
-export default Menu
+export default Menu;
