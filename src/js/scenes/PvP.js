7adcb6d69103086fb2c4ffde64387b7369a1c3d9
kyrstke <kyrstke@gmail.com>
2020-09-21 18:48:53 +0200

    Fixed players' random acceleration bug when hitting a wall

--- src/js/scenes/PvP.js
+++ src/js/scenes/PvP.js
@@ -6,7 +6,8 @@ import crossbar from '../../assets/crossbar.png';
 import net from '../../assets/net.png';
 import ball from '../../assets/ball.png';
 
-const vel_limit = 10;
+const vel_limit = 20;
+let i = 0;
 
 class PvP extends Phaser.Scene {
   constructor() {
@@ -19,7 +20,6 @@ class PvP extends Phaser.Scene {
     this.load.image('crossbar', crossbar);
     this.load.image('net', net);
     this.load.image('ball',ball);
-    
   }
 
   create() {
@@ -30,7 +30,7 @@ class PvP extends Phaser.Scene {
     this.player1.setScale(0.7, 0.7);
     this.player2 = this.physics.add.sprite(700, 700, 'wizball');
     this.player2.setScale(0.7, 0.7);
-    this.ball = this.physics.add.sprite(400, 100, 'ball');
+    this.ball = this.physics.add.sprite(400, 200, 'ball');
     this.ball.setScale(0.06, 0.06)
 
     this.player1.setCircle(46);
@@ -47,16 +47,20 @@ class PvP extends Phaser.Scene {
     this.player2.setCollideWorldBounds(true);
     this.ball.setCollideWorldBounds(true);
 
+    //this.ball.body.allowRotation = true;
+
     this.player1.setBounce(0.05);
-    //this.player1.setFriction(1, 1);
+    //this.player1.setFrictionX(1);
     this.player2.setBounce(0.05);
-    //this.player2.setFriction(1, 1);
+    //this.player2.setFrictionX(1);
     this.ball.setBounce(0.65);
-    //this.ball.setFriction(1, 1);
+    //this.ball.setFrictionX(1);
 
     this.player1.setMass(1);
     this.player2.setMass(1);
     this.ball.setMass(0.7);
+
+    this.ball.setMaxVelocity(800);
 
     this.physics.add.collider(this.player1, this.player2);
     this.physics.add.collider(this.ball, this.player2);
@@ -68,6 +72,7 @@ class PvP extends Phaser.Scene {
     this.A = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
     this.D = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
     this.W = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
+    
     this.net1 = this.add.sprite(755, 495, 'net');
     this.net2 = this.add.sprite(45, 495, 'net');
     this.net1.setScale(0.3, 0.3);
@@ -80,13 +85,11 @@ class PvP extends Phaser.Scene {
       this.player1.setVelocityX(0);
       this.player1.setAccelerationX(0);
     } 
-    if (this.cursors.left.isDown) {
-      this.player1.setAccelerationX(0);
+    else if (this.cursors.left.isDown) {
       this.player1.setVelocityX(-250);
       this.player1.setAccelerationX(600);
     } 
     else if (this.cursors.right.isDown) {
-      this.player1.setAccelerationX(0);
       this.player1.setVelocityX(250);
       this.player1.setAccelerationX(-600);        
     }
@@ -95,30 +98,31 @@ class PvP extends Phaser.Scene {
       this.player1.setAccelerationX(0);
       this.player1.setVelocityX(0);
     }
-
+    
     if (this.cursors.up.isDown && this.player1.body.blocked.down) {
       this.player1.setVelocityY(-350);
     } 
 
-    /* for(let i = 0; i < 1000; i++) {
-      console.log(this.player1.body.velocity.x);
-    } */
+
+    // Debug
+    /* if(i<10000) {
+      console.log(this.player1.body.velocity.x, 'player');
+      //console.log(this.ball.body.velocity.x);
+      i++;
+    } */ 
+
 
     if (this.A.isDown && this.D.isDown) {
       this.player2.setVelocityX(0);
       this.player2.setAccelerationX(0);
     } 
-
-    if (this.A.isDown) {
-      this.player2.setAccelerationX(0);
+    else if (this.A.isDown) {
       this.player2.setVelocityX(-250);
       this.player2.setAccelerationX(600);
     } 
     else if (this.D.isDown) {
-      this.player2.setAccelerationX(0);
       this.player2.setVelocityX(250);
       this.player2.setAccelerationX(-600);
-    
     } 
 
     if (Math.abs(this.player2.body.velocity.x) < vel_limit) {
@@ -145,7 +149,7 @@ class PvP extends Phaser.Scene {
         if(this.ball.body.blocked.down) {
           this.ball.setAccelerationX(100);
         } else {
-          this.ball.setAccelerationX(20) // TODO lower velocity.x by constant % when blocked.down and fix acceleration gained after hitting a wall
+          this.ball.setAccelerationX(20);
         }
       }
     } else {
